8b1f917353b982ec7d948efb08f3aae21004c20a
1997arief <1997arief@gmail.com>
2016-04-25 14:46:51 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <meta name="description" content=" Web OSKARD">
+        <meta name="keywords" content="OSKARD, oskard">
+        <meta name="author" content="Arief M. Ikhsan">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>OSKARD Project</title>
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <link rel="author" href="humans.txt">
+
+    </head>
+
+    <body>
+
+        <h1>OSKARD</h1>
+        <p>adalah singkatan dari <b>Organisasi Kemasyarakatan Remaja RT Delapan</b> yang berada di RT 08/RW 30 Nogotirto, Gamping, Sleman, DIY. Lahir pada tanggal 25 Desember 2012</p>
+
+        <script src="js/main.js"></script>
+
+    </body>
+
+</html>
